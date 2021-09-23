9394ce103dd07d6567c75528ecd27a8c36ba226f
Barend Noordhoff <barend@iagroup.app>
2021-09-23 16:26:44 +0200

    Drop down menu working

--- JGPlanning/resources/views/layouts/app.blade.php
+++ JGPlanning/resources/views/layouts/app.blade.php
@@ -21,12 +21,13 @@ The above copyright notice and this permission notice shall be included in all c
         {{--Css--}}
         <link rel="stylesheet" href="{{asset('/css/app.css')}}" type="text/css">
 
-
         <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
         <!--     Fonts and icons     -->
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
 
+
+{{--        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">--}}
     </head>
 
     <body>
@@ -100,31 +101,36 @@ The above copyright notice and this permission notice shall be included in all c
                                               </div>
                                             </li>-->
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" >
-                                        <i class="material-icons">person</i>
-                                        <p class="d-lg-none d-md-block">
-                                            Account
-                                        </p>
-                                    </a>
-                                    <div class="dropdown-menu" >
-                                        <a class="dropdown-item" href="#">Profile</a>
-                                        <a class="dropdown-item" href="#">Settings</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Log out</a>
-                                    </div>
+                                    <button class="dropdown_button" style="margin: 15px; font-size: 20px;" id="dropdown_button">
+                                        <i class="fas fa-caret-down" id="arrow" style="height: 100%"></i>
+                                        <i class="fa fa-user"></i>
+                                    </button>
+
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
+                <br>
+                <br>
+                <br>
+                <div class="dropdown_div">
+                    <div class="border shadow" style="display: none" id="dropdown_id">
+                        <div class="container">
+                            <br>
+                            <a href="#">Login</a> <hr>
+                            <a href="#">Settings</a> <hr>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
         {{--JS--}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+{{--        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>--}}
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script type="text/javascript" src="{{asset('/js/app.js')}}"></script>
     </body>
 </html>
