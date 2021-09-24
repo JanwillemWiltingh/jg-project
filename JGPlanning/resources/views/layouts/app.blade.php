23295f48c544b2777a4b1f0937d9630783af6b27
Barend Noordhoff <barend@iagroup.app>
2021-09-24 16:44:40 +0200

    Werkende routes naar login en dashboard

--- JGPlanning/resources/views/layouts/app.blade.php
+++ JGPlanning/resources/views/layouts/app.blade.php
@@ -121,18 +121,11 @@ The above copyright notice and this permission notice shall be included in all c
                         </div>
                     </div>
                 </div>
-
-{{--                <div class="dropdown_div">--}}
-{{--                    <div class="border shadow" style="display: none" id="dropdown_id">--}}
-{{--                        <div class="container">--}}
-{{--                            <br>--}}
-{{--                            <a href="#">Login</a> <hr>--}}
-{{--                            <a href="#">Help</a> <br>--}}
-{{--                            <br>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-
+                <div class="container">
+                    <div class="col-md-4">
+                        @yield('content')
+                    </div>
+                </div>
             </div>
         </div>
         {{--JS--}}
--- JGPlanning/routes/web.php
+++ JGPlanning/routes/web.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\Http\Controllers\ {
+    LoginController,
+    DashboardController,
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::get('/home', [DashboardController::class, 'index']);
