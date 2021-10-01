8429778da4a09c2816f35385880dc4cf6bf763ea
JanwillemWiltingh <123janwillem123@hotmail.nl>
2021-10-01 13:52:52 +0200

    Users table aangepast en clock table voor admin

--- JGPlanning/resources/views/admin/users/index.blade.php
+++ JGPlanning/resources/views/admin/users/index.blade.php
@@ -1,33 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <table style="">
-
-        <tr style="border: 1px solid darkgray; padding: 4px; text-align: center;">
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Id</th>
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Name</th>
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Email</th>
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Role_id</th>
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Role</th>
-            <th style="border: 1px solid darkgray; padding: 4px; text-align: center;">Active</th>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Role_id</th>
+            <th scope="col">Role</th>
+            <th scope="col">Active</th>
         </tr>
+        </thead>
+        <tbody>
         @foreach($users as $user)
-        <tr style="border: 1px solid darkgray; padding: 4px; text-align: center;">
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">{{$user['id']}}</td>
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">{{$user['name']}}</td>
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">{{$user['email']}}</td>
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">{{$user['role_id']}}</td>
-        @if($user['role_id'] == 1)
-                <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">Admin</td>
-            @else
-                <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">User</td>
-            @endif
-        @if(empty($user['deleted_at']))
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">Yes</td>
-            @else
-            <td style="border: 1px solid darkgray; padding: 4px; text-align: center;">No</td>
-            @endif
-        </tr>
+            <tr>
+                <td>{{$user['id']}}</td>
+                <td>{{$user['name']}}</td>
+                <td>{{$user['email']}}</td>
+                <td>{{$user['role_id']}}</td>
+                <td>{{$user->role()->get()->first()->name}}</td>
+                @if(empty($user['deleted_at']))
+                    <td >Yes</td>
+                @else
+                    <td>No</td>
+                @endif
+            </tr>
         @endforeach
+        </tbody>
     </table>
 @endsection
