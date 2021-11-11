774c62546acfac69fcc9e65ba75226fc80758792
JanwillemWiltingh <123janwillem123@hotmail.nl>
2021-11-11 13:15:23 +0100

    Pagination fucnte verwijdert uit de clock controller

--- JGPlanning/app/Http/Controllers/Users/ClockController.php
+++ JGPlanning/app/Http/Controllers/Users/ClockController.php
@@ -145,20 +145,4 @@ class ClockController extends Controller
             'entries' => (new CollectionPagination)->paginate($entries, 10, request('page'), ['path' => 'clock'])
         ]);
     }
-
-    /**
-     * @param $items
-     * @param int $perPage
-     * @param null $page
-     * @param array $options
-     * @return LengthAwarePaginator
-     */
-    public function paginate($items, int $perPage = 15, $page = null, array $options = []): LengthAwarePaginator
-    {
-        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
-
-        $items = $items instanceof Collection ? $items : Collection::make($items);
-
-        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
-    }
 }
