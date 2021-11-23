9907363095e9c4c521ed8e1ce2469b885113dbb9
Barend Noordhoff <barend@iagroup.app>
2021-11-23 13:35:52 +0100

    Dropdown fixes.

--- JGPlanning/public/js/app.js
+++ JGPlanning/public/js/app.js
@@ -5413,7 +5413,7 @@ $(document).ready(function () {
     window.location.href = "/rooster/" + this.value.substring(6) + '/' + this.value.slice(0, -4);
   });
   $('#manageDropdown').on('change', function () {
-    if (this.value == "Uitgezette dagen") {
+    if (this.value === "Uitgezette weken") {
       $('#DaysDiv').hide();
       $('#disabledDaysDiv').show();
     } else {
@@ -5422,7 +5422,7 @@ $(document).ready(function () {
     }
   });
   $('#weekDropdown').on('change', function () {
-    if (this.value == "Uitzetten") {
+    if (this.value === "Uitzetten") {
       $('#addWeeks').hide();
       $('#addDisable').show();
     } else {
