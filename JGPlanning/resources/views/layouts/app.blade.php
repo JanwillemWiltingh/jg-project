0d36e1a0d84213507304dc254007a20552305ca1
JanwillemWiltingh <123janwillem123@hotmail.nl>
2021-11-23 14:25:22 +0100

    Tab namen veranderd

--- JGPlanning/resources/views/layouts/app.blade.php
+++ JGPlanning/resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@ The above copyright notice and this permission notice shall be included in all c
                             <li class="nav-item active {{ (request()->is('gebruiker/clock') ?? request()->is('gebruiker/clock/*')) ? 'nav-color-active' : '' }}">
                                 <a class="nav-link nav-color" href="{{route('user.clock.index')}}" style="margin-top: 60px;">
                                     <i class="fa fa-clock"></i>
-                                    <p>Jouw Klok</p>
+                                    <p>Uw Klok</p>
                                 </a>
                             </li>
 {{--                        @endcan--}}
@@ -79,7 +79,7 @@ The above copyright notice and this permission notice shall be included in all c
                                 <li class="nav-item {{ (request()->is('rooster') or request()->is('rooster/*')) ? 'nav-color-active' : '' }}"  {{--style="position:absolute; left: 22px ;width: 90%"--}}>
                                     <a class="nav-link nav-color" href="{{route('rooster.index', ['week' => \Carbon\Carbon::now()->week, 'year' => \Carbon\Carbon::now('Y')->format('Y')])}}" style="margin-top: 120px;">
                                         <i class="fa fa-calendar" style="color: white"></i>
-                                        <p style="color: white">Rooster</p>
+                                        <p style="color: white">Uw Rooster</p>
                                     </a>
                                 </li>
 {{--                            </div>--}}
