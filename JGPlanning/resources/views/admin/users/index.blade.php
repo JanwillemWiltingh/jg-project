c76bd037b6c80d6986ae16ba4a7a61e6a685a810
Robert <robertpolman1217@gmail.com>
2021-12-14 11:39:57 +0100

    gebruiker informatie begin

--- JGPlanning/resources/views/admin/users/index.blade.php
+++ JGPlanning/resources/views/admin/users/index.blade.php
@@ -1,6 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
+    <script>
+        function getUserInfo(firstname, middlename, lastname, email, phone_number, created_at, updated_at, deleted_at){
+            document.getElementById('firstname').value = firstname;
+            document.getElementById('middlename').value = middlename;
+            document.getElementById('lastname').value = lastname;
+            document.getElementById('lastname').value = email;
+            document.getElementById('lastname').value = phone_number;
+            document.getElementById('lastname').value = created_at;
+            document.getElementById('lastname').value = updated_at;
+            document.getElementById('lastname').value = deleted_at;
+        }
+    </script>
+    <div class="modal fade" id="showUserInfo" tabindex="-1" role="dialog" aria-labelledby="a" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Gebruikers informatie</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-4">
+                            {{-- Firstname --}}
+                            <div class="form-group">
+                                <label class="black-label-text" for="firstname">Voornaam</label>
+                                <input type="text" class="form-control" id="firstname" value="@if(empty($user['firstname']))NULL @else{{$user['firstname']}} @endif" aria-describedby="firstname" placeholder="Voornaam" disabled>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            {{-- Middlename --}}
+                            <div class="form-group">
+                                <label class="black-label-text" for="middlename">Tussenvoegsel</label>
+                                <input type="text" class="form-control" id="middlename" value="@if(empty($user['middlename']))NULL @else{{$user['middlename']}} @endif" aria-describedby="middlename" placeholder="Tussenvoegsel" disabled>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            {{-- Lastname --}}
+                            <div class="form-group">
+                                <label class="black-label-text" for="lastname">Achternaam</label>
+                                <input type="text" class="form-control" id="lastname" value="@if(empty($user['lastname']))NULL @else{{$user['lastname']}} @endif" aria-describedby="lastname" placeholder="Achternaam" disabled>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Email --}}
+                    <div class="row">
+                        <div class="form-group">
+                            <label class="black-label-text" for="email">E-mail</label>
+                            <input type="email" class="form-control" id="email" value="@if(empty($user['email']))NULL @else{{$user['email']}} @endif" aria-describedby="email" placeholder="E-mail" disabled>
+                        </div>
+                    </div>
+                    {{-- Telefoonnummer --}}
+                    <div class="row">
+                        <div class="form-group">
+                            <label class="black-label-text" for="phone_number">Telefoonnummer</label>
+                            <input type="email" class="form-control" id="phone_number" value="@if(empty($user['phone_number']))NULL @else{{$user['phone_number']}} @endif" aria-describedby="phone_number" placeholder="Telefoonnummer" disabled>
+                        </div>
+                    </div>
+                    {{-- Last updated --}}
+                    <div class="row">
+                        <div class="form-group">
+                            <label class="black-label-text" for="updated_at">Laatst Bijgewerkt</label>
+                            <input type="text" class="form-control" id="updated_at" value="@if(empty($user['updated_at'])) - @else{{$user['updated_at']}} @endif" aria-describedby="updated_at" placeholder="Laatst Bijgewerkt" disabled>
+                        </div>
+                    </div>
+                    {{-- Last deleted --}}
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label class="black-label-text" for="created_at">Gebruiker Gecreëerd</label>
+                                <input type="text" class="form-control" id="created_at" value="@if(empty($user['created_at'])) - @else{{$user['created_at']}} @endif" aria-describedby="created_at" placeholder="Gebruiker Gecreëerd" disabled>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label class="black-label-text" for="updated_at">Gebruiker Verwijderd</label>
+                                <input type="text" class="form-control" id="updated_at" value="@if(empty($user['deleted_at'])) - @else{{$user['deleted_at']}} @endif" aria-describedby="updated_at" placeholder="Gebruiker Verwijderd" disabled>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <div class="fadeInDown crud-table">
 
     <text class="crud-user-form-title icon-color">Alle gebruikers</text><br>
@@ -34,18 +117,18 @@
             <tr class="{{ $user->isCurrentUser() }}">
 {{--                <th scope="row">{{ $loop->index + 1 }}</th>--}}
                 {{--Check the email from the current user and the email in the database to show who is selected(logged in)--}}
-                <td>{{$user['firstname']}}</td>
-                <td>{{ $user['middlename'] ?? '' }}</td>
-                <td>{{$user['lastname']}}</td>
+                <td style="cursor: pointer" href="#" data-bs-toggle="modal" data-bs-target="#showUserInfo" onclick="getUserInfo({{$user['firstname']}}, {{$user['middlename']}}, {{$user['lastname']}}, {{$user['email']}}, {{$user['phone_number']}}, {{$user['created_at']}}, {{$user['updated_at']}}, {{$user['deleted_at']}})">{{$user['firstname']}}</td>
+                <td style="cursor: pointer" href="#" data-bs-toggle="modal" data-bs-target="#showUserInfo" onclick="getUserInfo({{$user['firstname']}}, {{$user['middlename']}}, {{$user['lastname']}}, {{$user['email']}}, {{$user['phone_number']}}, {{$user['created_at']}}, {{$user['updated_at']}}, {{$user['deleted_at']}})">{{ $user['middlename'] ?? '' }}</td>
+                <td style="cursor: pointer" href="#" data-bs-toggle="modal" data-bs-target="#showUserInfo" onclick="getUserInfo({{$user['firstname']}}, {{$user['middlename']}}, {{$user['lastname']}}, {{$user['email']}}, {{$user['phone_number']}}, {{$user['created_at']}}, {{$user['updated_at']}}, {{$user['deleted_at']}})">{{$user['lastname']}}</td>
 
 {{--                <td>{{$user['email']}}</td>--}}
-                <td>{{$user['phone_number']}}</td>
+                <td style="cursor: pointer" href="#" data-bs-toggle="modal" data-bs-target="#showUserInfo" onclick="getUserInfo({{$user['firstname']}}, {{$user['middlename']}}, {{$user['lastname']}}, {{$user['email']}}, {{$user['phone_number']}}, {{$user['created_at']}}, {{$user['updated_at']}}, {{$user['deleted_at']}})">{{$user['phone_number']}}</td>
 
                 {{--Big letter maintainer--}}
 {{--                <td>@if($user['role_id'] == App\Models\Role::getRoleID('maintainer'))<strong>{{__('general.' .$user->role()->get()->first()->name)}}</strong> @else {{__('general.' .$user->role()->get()->first()->name)}} @endif</td>--}}
 
                 {{--Shows if the user is soft-deleted(active) or not--}}
-                <td>
+                <td style="cursor: pointer" href="#" data-bs-toggle="modal" data-bs-target="#showUserInfo" onclick="getUserInfo({{$user['firstname']}}, {{$user['middlename']}}, {{$user['lastname']}}, {{$user['email']}}, {{$user['phone_number']}}, {{$user['created_at']}}, {{$user['updated_at']}}, {{$user['deleted_at']}})">
                     @if(empty($user['deleted_at']))
                         Ja
                     @else
@@ -116,6 +199,7 @@
 {{--                    <a class="table-label" href="{{route('admin.users.show',$user['id'])}}" data-toggle="tooltip" title="Bekijken"><i class="fa-solid fa-user-gear icon-color"></i></a>--}}
 {{--                </td>--}}
             </tr>
+            </a>
         @endforeach
         </tbody>
     </table>
