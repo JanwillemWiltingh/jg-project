f5c34075609364b4e50e887d45910cf6f0da22c3
JanwillemWiltingh <123janwillem123@hotmail.nl>
2021-12-14 10:01:22 +0100

    Seeders aangepast voor testen en Dashboard uren aangepast

--- JGPlanning/database/seeders/ClockSeeder.php
+++ JGPlanning/database/seeders/ClockSeeder.php
@@ -21,25 +21,25 @@ class ClockSeeder extends Seeder
         Schema::enableForeignKeyConstraints();
 
         for ($i = 1; $i <= Carbon::now()->daysInMonth; $i++) {
-            if(Carbon::parse('2021-11-'.$i)->dayOfWeek != 0 and Carbon::parse('2021-11-'.$i)->dayOfWeek != 6) {
+            if(Carbon::parse('2021-12-'.$i)->dayOfWeek != 0 and Carbon::parse('2021-12-'.$i)->dayOfWeek != 6) {
                 DB::table('clocker')->insert([
                     'comment' => 'Seeded Comment '.$i,
                     'user_id' => 2,
                     'start_time' => rand(8, 12).':00:00',
                     'end_time' => rand(13, 18).':00:00',
-                    'date' => '2021-11-'.$i
+                    'date' => '2021-12-'.$i
                 ]);
             }
         }
 
         for ($i = 1; $i <= Carbon::now()->daysInMonth; $i++) {
-            if(Carbon::parse('2021-11-'.$i)->dayOfWeek != 0 and Carbon::parse('2021-11-'.$i)->dayOfWeek != 6) {
+            if(Carbon::parse('2021-12-'.$i)->dayOfWeek != 0 and Carbon::parse('2021-12-'.$i)->dayOfWeek != 6) {
                 DB::table('clocker')->insert([
                     'comment' => 'Seeded Comment '.$i,
                     'user_id' => 1,
                     'start_time' => rand(8, 12).':00:00',
                     'end_time' => rand(13, 18).':00:00',
-                    'date' => '2021-11-'.$i
+                    'date' => '2021-12-'.$i
                 ]);
             }
         }
