e0c1cb9de61428824b1b9a8764710903eaa9c12c
Barend Noordhoff <barend@iagroup.app>
2022-01-12 15:08:59 +0100

    Halfuur eraf gehaald gefixed, halve uren toegevoegd zodat de goede tijd word laten zien wanneer je uitklokt

--- JGPlanning/app/Models/Clock.php
+++ JGPlanning/app/Models/Clock.php
@@ -112,14 +112,7 @@ class Clock extends Model
 
         $time = Carbon::parse($end_time)->diffInSeconds($start_time);
         $final_time = (Ceil($time /3600 / .25)) * .25;
-        if ($final_time >= 5)
-        {
-            return number_format($final_time - .5, $decimal);
-        }
-        else
-        {
-            return number_format($final_time, $decimal);
-        }
+        return number_format($final_time, $decimal);
     }
 
     public function getUserData(string $field) {
